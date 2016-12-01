1d59baa658f71058be95e0469ded0409ce7c3590
JP Deguzman <jddeguzman@ucdavis.edu>
2016-12-01 15:06:18 -0800

    Added selecting images from the photo library and attaching them to the image view, changing the name assigned to the picture, and fixing up the UI

--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -18,15 +18,55 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
     }
     
-    // collectionView(_:numberOfItemsInSection:) must return an integer and tell the collection view how many items you want to show in its grid.
+    // collectionView(_:numberOfItemsInSection:) must return an integer and tell the collection view how many items you want to show in its grid. Here, we want to match the number of people we have entered.
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
     
     // collectionView(_:cellForItemAt:) must return an object of type UICollectionViewCell. We create and return the cell for the PersonCell class here.
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+        
+        // Pull out the person from the people array at the correct position
+        let person = people[indexPath.item]
+        
+        // Replace the cell name with the correct name
+        cell.name.text = person.name
+        
+        // Create a UIImage from the person's image filename and add it to the value from getDocumentsDirectory() so that we have a full path name
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        
+        // Adjust the borders
+        cell.imageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // Pull out the person object from the array index that was tapped
+        let person = people[indexPath.item]
+        
+        // Show an alert asking the user to rename the person
+        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        // Action to cancel the alert
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        // Action to save the new name by using a closure to pull out the text field value and assign to it the person's name property.
+        ac.addAction(UIAlertAction(title: "OK", style: .default) { [unowned self, ac] _ in
+            let newName = ac.textFields![0]
+            person.name = newName.text!
+            
+            // Update the collection view
+            self.collectionView?.reloadData()
+        })
+        
+        present(ac, animated: true)
     }
     
 
